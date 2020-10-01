79376a23eee0242909032e4e6ec380b4aa57fade
Samuel da Costa Araujo Nunes <samueldacostaaraujonunes@gmail.com>
2020-10-01 16:50:53 -0300

    Algumas ideias!

--- cal.py
+++ cal.py
@@ -1,41 +1,41 @@
-def soma (x,y): 
-	result = x + y
-	return print("A soma é igual: ", result)
+def soma (x, y): 
+	return x + y
 
-def sub (x,y): 
-	result = x - y
-	return print("A subtração é igual: ", result)
+def subtracao (x, y): 
+	return x - y
 
-def mul (x,y): 
-	result = int(x * y)
-	return print("A multiplicação é igual: ", result)
+def multiplicacao (x, y): 
+	return int(x * y)
 
-def div (x,y): 
-	result = x / y
-	return print("A soma é igual: ", result)
+def divisao (x, y): 
+	return x / y
+
+def calculadora (x, y, operacao):
+	result = operacao(x, y)
+	return print(f"A {operacao.__name__} é igual: {result}")
 
 cont = 0
 
 while cont ==0:
 
-	print("\n\n\n\nCalcuradora em Python\n\n")
-	print("Por favor, escolha uma das opições abaixo:\n")
-	print("1 - Adição (+)\n2 - Subtração (-)\n3 - Multiplicação (*)\n4 - Divição (/)")
+	print("\n\n\n\nCalculadora em Python\n\n")
+	print("Por favor, escolha uma das opções abaixo:\n")
+	print("1 - Adição (+)\n2 - Subtração (-)\n3 - Multiplicação (*)\n4 - Divisão (/)")
 
-	opicao = int(input("Qual operação você quer fazer? "))
-	if (opicao >=1) and (opicao <=4):
-		x = float(input("Digite o primeiro numero: "))
-		y = float(input("Digite o segundo numero: "))
-	else: print("Você não quer fazer nenhuma operação, thau!")
+	opcao = int(input("Qual operação você quer fazer? "))
+	if (opcao >=1) and (opcao <=4):
+		x = float(input("Digite o primeiro número: "))
+		y = float(input("Digite o segundo número: "))
+	else: print("Você não quer fazer nenhuma operação, tchau!")
 
-	if opicao == 1:
-		soma(x,y)
-	elif opicao == 2:
-		sub(x,y)
-	elif opicao == 3:
-		mul(x,y)
-	elif opicao == 4:
-		div(x,y)
+	if opcao == 1:
+		calculadora(x, y, soma)
+	elif opcao == 2:
+		calculadora(x, y, subtracao)
+	elif opcao == 3:
+		calculadora(x, y, multiplicacao)
+	elif opcao == 4:
+		calculadora(x, y, divisao)
 
 	conf = 3
 	while (conf < 0) or ( conf > 2): 
@@ -44,10 +44,10 @@ while cont ==0:
 			print("Vamos lá")
 			conf = resp
 		elif resp == 1:
-			print("Você não quer fazer nenhuma operação, thau!")
+			print("Você não quer fazer nenhuma operação, tchau!")
 			conf = resp
 			cont = 1
 		else:
-			print("Opição invalida!")
+			print("Opção inválida!")
 
 
